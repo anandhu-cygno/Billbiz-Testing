f7960785a358a9f5c21de96dac4997c23864e9b1
aman10rasheed <amanrasheedpro@gmail.com>
2024-07-08 09:26:37 +0000

    micros

---
+++ Purchase/controller/accountController.js
@@ -0,0 +1,84 @@
+const Organization = require("../database/model/organization");
+const Account = require("../database/model/account")
+
+
+//Add Account
+exports.addAccount = async (req, res) => {
+    console.log("Add Account:", req.body);
+    try {
+      const {       
+        organizationId,
+        accountName,
+        accountCode,
+        accountType,
+        accountGroup,
+        accountHeads,
+        openingBalance,
+        openingBalanceDate,
+        description,
+        bankAccNum,
+        bankIfsc,
+        bankCurrency,
+      } = req.body;
+  
+      // Check if an organization with the same organizationName already exists
+      const existingAccount = await Account.findOne({ accountName });
+  
+      if (existingAccount) {
+        return res.status(409).json({
+          message: "Account with the provided Account Name already exists.",
+        });
+      }
+  
+      // Create a new organization
+      const newAccount = new Account({
+        organizationId,
+        accountName,
+        accountCode,
+        accountType,
+        accountGroup,
+        accountHeads,
+        openingBalance,
+        openingBalanceDate,
+        description,
+        bankAccNum,
+        bankIfsc,
+        bankCurrency,        
+      });
+  
+      
+      await newAccount.save();
+  
+      
+      res.status(201).json({
+        message: "Account created successfully."
+      });
+      console.log("Account created successfully:");
+    } catch (error) {
+      console.error("Error creating Account:", error);
+      res.status(500).json({ message: "Internal server error." });
+    }
+  };
+
+
+// Get all accounts for a given organizationId
+exports.getOneAccount = async (req, res) => {
+    try {
+        const { organizationId } = req.body;
+        // console.log(organizationId);
+
+        // Find all accounts where organizationId matches
+        const accounts = await Account.find({ organizationId });
+
+        if (!accounts.length) {
+            return res.status(404).json({
+                message: "No accounts found for the provided organization ID.",
+            });
+        }
+
+        res.status(200).json(accounts);
+    } catch (error) {
+        console.error("Error fetching accounts:", error);
+        res.status(500).json({ message: "Internal server error." });
+    }
+};
---
+++ Purchase/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const cors = require('cors')
+
+const server = express()
+
+const accountRouter = require("./router/accountRouter")
+
+require('./database/connection/connection')
+
+server.use(cors())
+
+server.use(express.json())
+
+server.use(accountRouter)
+
+PORT = 3003
+
+server.get('/',(req,res)=>{
+    res.status(200).json("Bill BIZZ server started")
+})
+
+server.listen(PORT,()=>{
+    console.log(`BillBIZZ server started at port : ${PORT}`);
+})
+
