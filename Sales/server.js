4beb0e4b06b701081f1c012bca256e7cfd30d0b7
Devgnoz <dev@cygnoz.com>
2024-07-10 15:53:42 +0530

    Update server.js

--- Sales/server.js
+++ Sales/server.js
@@ -6,11 +6,8 @@ const cors = require('cors')
 
 const server = express()
 
-<<<<<<< HEAD
-const saleRouter = require("./router/saleRouter")
-=======
 const salesRouter = require("./router/salesRouter")
->>>>>>> d9002ce8a4237c5bb8f605d02048c64475165e4b
+
 
 require('./database/connection/connection')
 
@@ -18,18 +15,6 @@ server.use(cors())
 
 server.use(express.json())
 
-<<<<<<< HEAD
-server.use(saleRouter)
-
-PORT = 3007
-
-server.get('/',(req,res)=>{
-    res.status(200).json("Bill BIZZ server started")
-})
-
-server.listen(PORT,()=>{
-    console.log(`BillBIZZ server started at port : ${PORT}`);
-=======
 server.use(salesRouter)
 
 PORT = 5007
@@ -40,6 +25,5 @@ server.get('/',(req,res)=>{
 
 server.listen(PORT,()=>{
     console.log(`BillBIZZ server Sales started at port : ${PORT}`);
->>>>>>> d9002ce8a4237c5bb8f605d02048c64475165e4b
 })
 
