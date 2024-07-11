7b82db49e8f754e43e6fdcd1dbc58a92de908fc3
MohdThaha <thahad2@gmail.com>
2024-07-11 10:07:07 +0530

    trail

---
+++ Accounts/database/model/trialBalence.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+
+const trialBalenceSchema = new Schema({
+    organizationId: {type:String},
+    transaction_id: {type:String},
+    date: {type:String},
+    account_id: {type:String},
+    accountName: {type:String},
+    action: {type:String},
+    debitAmount: {type:String},
+    creditAmount: {type:String},
+    balence: {type:String},
+    remark: {type:String},
+});
+
+const TrialBalences = mongoose.model("TrialBalences", trialBalenceSchema);
+
+module.exports = TrialBalences;
