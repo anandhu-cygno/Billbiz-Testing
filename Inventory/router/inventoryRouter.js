1a2e4ae32b02810222e0256e67abd2321dde74a0
Anju <anjup3994@gmail.com>
2024-07-16 10:24:36 +0530

    unit

--- Inventory/router/inventoryRouter.js
+++ Inventory/router/inventoryRouter.js
@@ -2,17 +2,31 @@ const express = require("express")
 
 const router = new express.Router()
 
-<<<<<<< HEAD
-const itemController = require("../controller/itemController")
+const itemController = require("../controller/itemController");
+const unitController = require("../controller/unitController")
 
 // Item
-router.post('/item/add', itemController.addItem);
-router.get('/item/get-all', itemController.getAllItem);
-router.get('/item/get/:id', itemController.getAItem)
-router.put('/item/update/:id',itemController.updateItem)
-router.delete('/item/delete/:id',itemController.deleteItem)
-=======
->>>>>>> d9002ce8a4237c5bb8f605d02048c64475165e4b
+router.post('/add-item', itemController.addItem);
+router.get('/get-all-item', itemController.getAllItem);
+router.get('/get-one-item/:id', itemController.getAItem)
+router.put('/edit-item',itemController.updateItem)
+router.delete('/delete-item/:id',itemController.deleteItem)
+
+
+// Unit
+router.post('/add-unit', unitController.addUnit);
+router.get('/get-all-unit', unitController.getAllUnit);
+router.get('/get-one-unit/:_id', unitController.getOneUnit);
+router.put('/edit-unit', unitController.updateUnit);
+router.delete('/delete-unit/:id', unitController.deleteUnit);
+// Unit Conversion
+router.post('/add-unitConversion', unitController.addUnitConversion);
+router.get('/get-all-unitConversion', unitController.getAllUnitConversion);
+router.get('/get-one-unitConversion/:_id', unitController.getOneUnitConversion);
+router.put('/edit-unitConversion', unitController.updateUnitConversion);
+router.delete('/delete-unitConversion/:id', unitController.deleteUnitConversion);
+
+
 
 
 module.exports = router
